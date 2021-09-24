77c0997f54fe38c3fd29a503e77eb600ace24421
adrskora <adrskora@gmail.com>
2021-09-24 09:06:12 +0000

    first commit

---
+++ Math.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace MathSolution
+{
+    class Math
+    {
+        public int Add(int a, int b)
+        {
+            return a+b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a-b;
+        }
+        public int Div(int a, int b)
+        {
+            if(b==0)
+                throw new DivideByZeroException();
+            return  a/b;
+        }
+        public int Mul(int a, int b)
+        {
+            return a*b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MathSolution
+{
+    class Program
+    {
+        private static Math math= new Math();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 + 1 = "+math.Add(1,1));
+        }
+    }
+}
